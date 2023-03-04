790f4d3e5062a21497a90751a50887f0260a42b8
GustavoCassel <gustavo.cassel.52@gmail.com>
2023-03-04 18:00:55 -0300

    Add project files.

---
+++ BankProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
